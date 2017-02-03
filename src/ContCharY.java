87589cd5f4772a5f111a0796992b3dd5211e000b
Anilla <Anilla@172.32.3.148>
2017-02-03 11:52:27 +0000

    Prueba de ETS

---
+++ src/ContCharY.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+public class ContCharY {
+		
+	public static void main(String[] args) throws IOException {
+			
+		System.out.println("Por favor, introduzca carácteres siendo 0 para parar de introducir: ");
+			boolean repetir = true;
+			Scanner teclado = new Scanner(System.in);
+			
+			int ContY = 0;
+			
+			while (repetir){
+			char c = (char) System.in.read();
+			int asc_c = (int) c;
+			
+			if (asc_c == 89 || asc_c == 121){
+				ContY++;
+				}
+			if (asc_c == 48){
+				System.out.print("Ha introducido el carácter 'Y' " +ContY +" veces.");
+				repetir = false;
+			}
+			}
+			teclado.close();	
+			//prueba de ETS
+			}
+		}
